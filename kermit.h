1478aae509f216ad3db05eaf1d9d771a88d37560
Kenji Rikitake <kenji.rikitake@acm.org>
2025-03-26 16:27:21 +0900

    kermit.h: set P_PKTLEN to 512 bytes

--- kermit.h
+++ kermit.h
@@ -67,7 +67,7 @@
 #ifdef NEO6502
 #define NO_SCAN /* We don't need F_SCAN, transparent file only */
 #define FN_MAX 16
-#define P_PKTLEN 256
+#define P_PKTLEN 512
 #endif /* NEO6502 */
 
 /* XAC compiler for Philips XAG30 microprocessor */
